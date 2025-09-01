184c7eceadacdac1b0f06bef6eaf55c84c5cf6b2
DavidSebi27 <730630@student.inholland.nl>
2025-09-01 19:16:23 +0200

    Basically first tried it thats gotta count for something

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of students in the group: ");
+        Integer numberOfStudents = input.nextInt();
+        Student[] students = new Student[numberOfStudents];
 
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            System.out.printf("Enter the name of the student #%d ", (i + 1));
+            String name = input.next();
+            students[i] = new Student(name);
+        }
+
+        int nr = 1;
+        for (Student student : students)
+        {
+            System.out.println("Student #" + nr + ": " + student.name);
+            nr++;
+        }
+
+        for  (Student student : students)
+        {
+            System.out.printf("Is %s present? [Y/N]: ", student.name);
+            boolean isPresent = input.next().equals("Y");
+            if(isPresent)
+            {
+                student.presence = true;
+            }
+        }
+
+        nr = 1;
+        for (Student student : students)
+        {
+            System.out.printf("Student #%d: %-10s| Present: %s\n", nr, student.name, (student.presence) ? "True" : "False");
+            nr++;
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student
+{
+    public String name;
+    public boolean presence;
+
+    public Student(String name)
+    {
+        this.name = name;
+    }
+
+    public Student(String name, boolean presence)
+    {
+        this.name = name;
+        this.presence = presence;
+    }
+}
